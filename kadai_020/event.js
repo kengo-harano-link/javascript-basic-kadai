cdc320d9d9c44801cd1d29ddb35858c887fe8ff3
kengo-harano-link <k-harano@link-si.co.jp>
2026-01-28 21:26:12 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+ const addBtn = document.getElementById('btn');
+ const text = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+  text.textContent = "ボタンをクリックしました";
+});
